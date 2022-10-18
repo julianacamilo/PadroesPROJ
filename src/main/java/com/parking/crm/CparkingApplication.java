e22b9e44232ba6bd0f0f4fb792915d6f766400c6
Juliana Camilo <104179960+julianacamilo@users.noreply.github.com>
2022-10-18 12:17:11 -0300

    Add files via upload

---
+++ src/main/java/com/parking/crm/CparkingApplication.java
@@ -0,0 +1,13 @@
+package com.parking.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CparkingApplication.class, args);
+	}
+
+}
